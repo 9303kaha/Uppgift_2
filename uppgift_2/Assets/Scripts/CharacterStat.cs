27f3b8d5329a69143c7f5ab2b884e08b7f667788
9303kaha <9303kaha@hotmail.se>
2015-04-29 12:25:13 +0200

    Jobb
    Jobb

---
+++ uppgift_2/Assets/Scripts/CharacterStat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStat : MonoBehaviour {
+
+	public int characterType = 0; //1 for Player 1, 2 for Player 2, 3 for Monster
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
